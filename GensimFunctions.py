534dd98deb5f68163e4db03751425d91af9a0e41
manikamittal <manikamittal22@gmail.com>
2016-05-24 18:27:23 -0700

    Added Word2Vec

--- GensimFunctions.py
+++ GensimFunctions.py
@@ -1,19 +1,22 @@
 from gensim import corpora, models, similarities
 
+
 def create_dictionary(rawSamples):
     texts = [[word for word in question[0].lower().split()] for question in rawSamples]
     dictionary = corpora.Dictionary(texts)
     dictionary.save('gensim_dictionary.dict')
     corpus = [dictionary.doc2bow(text) for text in texts]
     corpora.MmCorpus.serialize('gensim_corpus.mm', corpus)
-    return (dictionary,corpus)
+    return (dictionary, corpus)
+
 
 def load_dictcorpus():
     dictionary = corpora.Dictionary.load('gensim_dictionary.dict')
     corpus = corpora.MmCorpus('gensim_corpus.mm')
-    return (dictionary,corpus)
+    return (dictionary, corpus)
 
-def get_similarity(corpus, model, index, key1, key2,keyToIndex):
+
+def get_similarity(corpus, model, index, key1, key2, keyToIndex):
     i = keyToIndex[key1]
     vecLsi = model[corpus[i]]
     similarQuestions = index[vecLsi]
@@ -21,74 +24,86 @@ def get_similarity(corpus, model, index, key1, key2,keyToIndex):
     testIndex = keyToIndex[key2]
     return similarQuestions[testIndex][1]
 
+
 # LSI MODEL
 def create_lsimodelindex(dictionary, corpus, numOfTopics):
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=numOfTopics)
     lsi.save('gensim_lsimodel.lsi')
     index = similarities.MatrixSimilarity(lsi[corpus])
     index.save('gensim_lsiindex.index')
-    return (lsi,index)
+    return (lsi, index)
+
 
 def load_lsimodelindex():
     lsiModel = models.LsiModel.load('gensim_lsimodel.lsi')
     lsiIndex = similarities.MatrixSimilarity.load('gensim_lsiindex.index')
-    return (lsiModel,lsiIndex)
+    return (lsiModel, lsiIndex)
 
-def get_lsisim(data,corpus,key1,key2):
-    (lsiModel,lsiIndex) = load_lsimodelindex()
-    similarityScore = get_similarity(corpus,lsiModel,lsiIndex,key1,key2,data.keyToIndex)
+
+def get_lsisim(data, corpus, key1, key2):
+    (lsiModel, lsiIndex) = load_lsimodelindex()
+    similarityScore = get_similarity(corpus, lsiModel, lsiIndex, key1, key2, data.keyToIndex)
     return similarityScore
 
+
 # LDA MODEL
-def create_ldamodelindex(dictionary,corpus,numOfTopics):
+def create_ldamodelindex(dictionary, corpus, numOfTopics):
     lda = models.LdaModel(corpus, id2word=dictionary, num_topics=numOfTopics)
     lda.save('gensim_ldamodel.lda')
     index = similarities.MatrixSimilarity(lda[corpus])
     index.save('gensim_ldaindex.index')
-    return (lda,index)
+    return (lda, index)
+
 
 def load_ldamodelindex():
     ldaModel = models.LdaModel.load('gensim_ldamodel.lda')
     ldaIndex = similarities.MatrixSimilarity.load('gensim_ldaindex.index')
-    return (ldaModel,ldaIndex)
+    return (ldaModel, ldaIndex)
 
-def get_ldasim(data,corpus,key1,key2):
-    (ldaModel,ldaIndex) = load_ldamodelindex()
-    similarityScore = get_similarity(corpus,ldaModel,ldaIndex,key1,key2,data.keyToIndex)
+
+def get_ldasim(data, corpus, key1, key2):
+    (ldaModel, ldaIndex) = load_ldamodelindex()
+    similarityScore = get_similarity(corpus, ldaModel, ldaIndex, key1, key2, data.keyToIndex)
     return similarityScore
 
+
 # RP MODEL
-def create_rpmodelindex(dictionary,corpus,numOfTopics):
+def create_rpmodelindex(dictionary, corpus, numOfTopics):
     rp = models.RpModel(corpus, num_topics=numOfTopics)
     rp.save('gensim_rpmodel.rp')
     index = similarities.MatrixSimilarity(rp[corpus])
     index.save('gensim_rpindex.index')
-    return (rp,index)
+    return (rp, index)
+
 
 def load_rpmodelindex():
     rpModel = models.RpModel.load('gensim_rpmodel.rp')
     rpIndex = similarities.MatrixSimilarity.load('gensim_rpindex.index')
-    return (rpModel,rpIndex)
+    return (rpModel, rpIndex)
 
-def get_rpsim(data,corpus,key1,key2):
-    (rpModel,rpIndex) = load_rpmodelindex()
-    similarityScore = get_similarity(corpus,rpModel,rpIndex,key1,key2,data.keyToIndex)
+
+def get_rpsim(data, corpus, key1, key2):
+    (rpModel, rpIndex) = load_rpmodelindex()
+    similarityScore = get_similarity(corpus, rpModel, rpIndex, key1, key2, data.keyToIndex)
     return similarityScore
 
+
 # HDP MODEL
-def create_hdpmodelindex(dictionary,corpus):
+def create_hdpmodelindex(dictionary, corpus):
     hdp = models.HdpModel(corpus, id2word=dictionary)
     hdp.save('gensim_hdpmodel.hdp')
     index = similarities.MatrixSimilarity(hdp[corpus])
     index.save('gensim_hdpndex.index')
-    return (hdp,index)
+    return (hdp, index)
+
 
 def load_hdpmodelindex():
     hdpModel = models.HdpModel.load('gensim_hdpmodel.hdp')
     hdpIndex = similarities.MatrixSimilarity.load('gensim_hdpindex.index')
-    return (hdpModel,hdpIndex)
+    return (hdpModel, hdpIndex)
 
-def get_hdpsim(data,corpus,key1,key2):
-    (hdpModel,hdpIndex) = load_hdpmodelindex()
-    similarityScore = get_similarity(corpus,hdpModel,hdpIndex,key1,key2,data.keyToIndex)
+
+def get_hdpsim(data, corpus, key1, key2):
+    (hdpModel, hdpIndex) = load_hdpmodelindex()
+    similarityScore = get_similarity(corpus, hdpModel, hdpIndex, key1, key2, data.keyToIndex)
     return similarityScore
--- word2vec.py
+++ word2vec.py
@@ -1,10 +1,51 @@
 from gensim import models
+from Data import Data
+import numpy as np
+from scipy import spatial
+from calcAccuracy import test_accuracy
 
 
-def testing():
-    model = models.Word2Vec.load_word2vec_format('googleWord2Vec.bin', binary=True)
-    yolo = model.most_similar(positive=['woman', 'king'], negative=['man'])
-    print yolo
+def isSimilar(model, questionText1, questionText2):
+    question1_vector = np.zeros((300), dtype=np.float32)
+    question2_vector = np.zeros((300), dtype=np.float32)
+    for word in questionText1.split():
+        try:
+            question1_vector += model[word]
+        except:
+            pass
+    for word in questionText2.split():
+        try:
+            question2_vector += model[word]
+        except:
+            pass
+    cosine_sim = 1 - spatial.distance.cosine(question1_vector, question2_vector)
+    print cosine_sim
+    if cosine_sim > 0.90:
+        return 1
+    return 0
 
 
-testing()
+def fire_word2vec(data, model, filename):
+    f = open(filename, "w")
+    for i in range(0, len(data.testData)):
+        print ">>> Data Sample " + str(i + 1)
+        question1Index = data.keyToIndex[data.testData[i][0]]
+        question2Index = data.keyToIndex[data.testData[i][1]]
+        questionText1 = data.rawSamples[question1Index][0]
+        questionText2 = data.rawSamples[question2Index][0]
+        duplicate = isSimilar(model, questionText1, questionText2)
+        f.write(data.testData[i][0] + " " + data.testData[i][1] + " " + str(duplicate) + "\n")
+    f.close()
+
+
+data = Data()
+data.load_statistics()
+data.parse_questions_no_stemming()
+
+model = models.Word2Vec.load_word2vec_format('googleWord2Vec.bin', binary=True)
+
+filename = 'sample_output_word2vec.out'
+fire_word2vec(data, model, filename)
+
+print('---------------------------')
+test_accuracy(filename)
