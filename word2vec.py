ca9c6532bcc5c068f2f3848f73f5264636d0d8d1
nisargmodi <nisarg92modi@gmail.com>
2016-05-24 22:15:46 -0700

    csv file

--- word2vec.py
+++ word2vec.py
@@ -3,9 +3,10 @@ from Data import Data
 import numpy as np
 from scipy import spatial
 from calcAccuracy import test_accuracy
+import csv
 
 
-def isSimilar(model, questionText1, questionText2):
+def isSimilar(model, questionText1, questionText2, filename):
     question1_vector = np.zeros((300), dtype=np.float32)
     question2_vector = np.zeros((300), dtype=np.float32)
     for word in questionText1.split():
@@ -20,10 +21,13 @@ def isSimilar(model, questionText1, questionText2):
             pass
     cosine_sim = 1 - spatial.distance.cosine(question1_vector, question2_vector)
     print cosine_sim
+    list=[questionText1,questionText2,cosine_sim]
+    with open(filename, "a") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        wr.writerow(list)
     if cosine_sim > 0.90:
         return 1
     return 0
-
 
 def fire_word2vec(data, model, filename):
     f = open(filename, "w")
@@ -33,11 +37,16 @@ def fire_word2vec(data, model, filename):
         question2Index = data.keyToIndex[data.testData[i][1]]
         questionText1 = data.rawSamples[question1Index][0]
         questionText2 = data.rawSamples[question2Index][0]
-        duplicate = isSimilar(model, questionText1, questionText2)
+        duplicate = isSimilar(model, questionText1, questionText2, 'cosine_similarity.csv')
         f.write(data.testData[i][0] + " " + data.testData[i][1] + " " + str(duplicate) + "\n")
     f.close()
 
-
+def csv_write_test():
+    list=["questionText1","questionText2",0]
+    with open(filename, "a") as fp:
+        wr = csv.writer(fp, dialect='excel')
+        wr.writerow(list)
+    
 data = Data()
 data.load_statistics()
 data.parse_questions_no_stemming()
